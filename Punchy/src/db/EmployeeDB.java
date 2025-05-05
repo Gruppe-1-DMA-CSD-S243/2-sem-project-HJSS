33f9c2540c710b68cb9104247c7d0d02879ccd7e
JonasHovaldt <10632099@ucn.dk>
2025-05-05 13:01:04 +0200

    Employee setup
    Setup of classes for Employee funtionality:
    EmployeeController and EmployeeControllerIF
    EmployeeDB and Employee DBIF
    Employee

--- Punchy/src/db/EmployeeDB.java
+++ Punchy/src/db/EmployeeDB.java
@@ -1,5 +1,35 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.Employee;
+
 public class EmployeeDB implements EmployeeDBIF {
+	private static final String FIND_EMPLOYEE_QUERY = "SELECT * FROM Employee WHERE employee_number = ?";
+	private PreparedStatement findEmployeePS;
+	
+	public EmployeeDB() {
+		init();
+	}
+	
+	private void init() {
+		// TODO Auto-generated method stub
+		Connection con = DBConnection.getInstance().getDBcon();
+		try {
+			findEmployeePS = con.prepareStatement(FIND_EMPLOYEE_QUERY);
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Employee findEmployee(String employeeNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
