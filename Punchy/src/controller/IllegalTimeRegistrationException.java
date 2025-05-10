50ad3f4f1f6e66d0de2ceed282d50cdf5b986a6b
GitGood <10503084@ucn.dk>
2025-05-10 20:03:00 +0200

    Tweaks
    Tweaks

---
+++ Punchy/src/controller/IllegalTimeRegistrationException.java
@@ -0,0 +1,5 @@
+package controller;
+
+public class IllegalTimeRegistrationException extends Exception{
+	public IllegalTimeRegistrationException( String e ) { super(e); }
+}
--- Punchy/src/utility/ValidateTimeRegistration.java
+++ Punchy/src/utility/ValidateTimeRegistration.java
@@ -1,6 +1,47 @@
 package utility;
 
+import java.time.LocalDateTime;
+
+import controller.IllegalTimeRegistrationException;
+import model.TimeRegistration;
+
 public class ValidateTimeRegistration {
 	
-	// FLYT CLOCKOUT LOGIK FRA GUI TIL VALIDATE KLASSE
+	// FLYT EndTime LOGIK FRA GUI TIL VALIDATE KLASSE
+	public static void validateData(TimeRegistration timeRegistration) throws IllegalTimeRegistrationException {
+		
+	}
+	
+	public static boolean validateStartTimeBeforeEndTime(LocalDateTime startTime, LocalDateTime endTime) {
+		boolean startTimeIsBeforeEndTime = false;
+		
+		if (startTime.isBefore(endTime)) {
+			startTimeIsBeforeEndTime = true;
+		}
+		
+		return startTimeIsBeforeEndTime;
+	}
+	
+	public static boolean validateStartTime(LocalDateTime startTime) {
+		boolean startTimeIsValid = false;
+		
+		if (startTime != null) {
+			startTimeIsValid = true;
+		}
+		
+		return startTimeIsValid;
+	}
+	
+	public static boolean validateEndTime(LocalDateTime endTime) {
+		boolean endTimeIsValid = false;
+		
+		if (endTime != null) {
+			endTimeIsValid = true;
+		}
+		
+		return endTimeIsValid;
+	}
+	
+	
+	
 }
