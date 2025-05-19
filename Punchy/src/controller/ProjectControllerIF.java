94155eeada0eba7c079a460703c19762e04d2d06
hhkringel <hhkr1989@gmail.com>
2025-05-19 11:11:28 +0200

    Update ProjectControllerIF.java
    Javadoc for ProjectControllerIF

--- Punchy/src/controller/ProjectControllerIF.java
+++ Punchy/src/controller/ProjectControllerIF.java
@@ -5,6 +5,10 @@ import java.util.List;
 import model.Employee;
 import model.Project;
 
+/** Purpose of this interface is to make a blueprint for the ProjectController class <br> 
+ * @author Henrik Holmberg Kringel
+ */
+
 public interface ProjectControllerIF {
 
 	public Project findProject(String projectNumber, String employeeNumber);
